0669f7748f9a0b0e88a78b3b120bee4a73368148
sanjay3081 <84510522+sanjay3081@users.noreply.github.com>
2021-09-02 18:11:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,12 +11,12 @@ access_token_secret=os.getenv('access_token_secret')
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 
-api = tweepy.API(auth)
+api = tweepy.API(auth)  
 print("Authentication approved")
 while True:
   user = api.get_user('@Ameen91741779')
   u = user.followers_count
   api.update_profile(name=f'AMEER {u} Followers')
   print(f'AMEER {u} Followers')
-  time.sleep(60) 
+  time.sleep(60)  
  
